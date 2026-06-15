2e022780fdcbc364265a4576f34cf6705be3dade
chathamabate <chathamabate@gmail.com>
2026-06-14 19:46:14 -0700

    Slowly getting somewhere

--- src/fernconf/ConfigSchema.py
+++ src/fernconf/ConfigSchema.py
@@ -3,64 +3,108 @@ from abc import ABCMeta, abstractmethod
 from enum import Enum
 import re
 from typing import ClassVar, Any
+from result import Ok, Err, Result
 
-class FCValue(metaclass=ABCMeta):
-    @staticmethod
-    def of(value: Any) -> FCValue | None:
-        match value:
-            case int():
-                return FCValueInt(value)
-            case bool():
-                return FCValueBool(value)
-            case str():
-                return FCValueStr(value)
-            case dict():
-                new_dict: dict[str, FCValue] = {}
-                for k, v in value.items():
-                    if not isinstance(k, str):
-                        return None
+type FCValue = int | bool | str | dict[str, FCValue]
 
-                    new_val = FCValue.of(v)
-                    if new_val is None:
-                        return None
+FCVALUE_KEY_PATTERN: re.Pattern = re.compile("[A-Za-z_][A-Za-zZ0-9_]*")
 
-                    new_dict[k] = new_val
+def of(value: Any) -> Result[FCValue, str]:
+    """
+    The purpose of this function is to "construct" a FCValue from an any typed value.
+    You may source a value from something which cannot be typechecked before runtime.
+    (For, example a JSON file)
 
-                return FCValueObj(new_dict)
-            case _:
-                return None
-        
-class FCValueInt(FCValue):
-    def __init__(self, value: int):
-        self.value = value
+    This file creates a deep copy of `value` on success.
+    """
+    match value:
+        case int() | bool() | str():
+            return Ok(value)
+        case dict():
+            new_dict: dict[str, FCValue] = {}
+            for k, v in value.items():
+                if not isinstance(k, str):
+                    return Err("dict values must only have string keys")
 
-    def __str__(self) -> str:
-        return str(self.value)
+                if not FCVALUE_KEY_PATTERN.fullmatch(k):
+                    return Err(f"dict key name does not conform to FCValue regex: \"{k}\"")
 
-class FCValueBool(FCValue):
-    def __init__(self, value: bool):
-        self.value = value
+                match of(v):
+                    case Ok(new_val):
+                        new_dict[k] = new_val
+                    case Err(msg):
+                        return Err(f"[{k}] {msg}")
 
-    def __str__(self) -> str:
-        return str(self.value)
+            return Ok(new_dict)
+        case _:
+            return Err("FCValues must conform to typedef: int | bool | str | dict[str, FCValue]")
 
-class FCValueStr(FCValue):
-    def __init__(self, value: str):
-        self.value = value
 
-    def __str__(self) -> str:
-        return str(self.value)
 
-class FCValueObj(FCValue):
-    def __init__(self, value: dict[str, FCValue]):
-        self.value = value
+#class FCValue(metaclass=ABCMeta):
+#    @staticmethod
+#    def of(value: Any) -> FCValue | None:
+#        match value:
+#            case int():
+#                return FCValueInt(value)
+#            case bool():
+#                return FCValueBool(value)
+#            case str():
+#                return FCValueStr(value)
+#            case dict():
+#                new_dict: dict[str, FCValue] = {}
+#                for k, v in value.items():
+#                    if not isinstance(k, str):
+#                        return None
+#
+#                    new_val = FCValue.of(v)
+#                    if new_val is None:
+#                        return None
+#
+#                    new_dict[k] = new_val
+#
+#                return FCValueObj(new_dict)
+#            case _:
+#                return None
+#        
+#class FCValueInt(FCValue):
+#    def __init__(self, value: int):
+#        self.value = value
+#
+#    def __str__(self) -> str:
+#        return str(self.value)
+#
+#class FCValueBool(FCValue):
+#    def __init__(self, value: bool):
+#        self.value = value
+#
+#    def __str__(self) -> str:
+#        return str(self.value)
+#
+#class FCValueStr(FCValue):
+#    def __init__(self, value: str):
+#        self.value = value
+#
+#    def __str__(self) -> str:
+#        return str(self.value)
+#
+#class FCValueObj(FCValue):
+#    def __init__(self, value: dict[str, FCValue]):
+#        self.value = value
+#
+#    def __str__(self) -> str:
+#        return "{" + ", ".join([f"\"{k}\": {str(v)}"   for k, v in self.value.items()]) + "}"
 
-    def __str__(self) -> str:
-        return "{" + ", ".join([f"\"{k}\": {str(v)}"   for k, v in self.value.items()]) + "}"
-
+# IDK, are there even benefits to this design above tbh??
+# I don't even really know at this point?
+# Honestly, I don't even really know??
+# Well, I guess there are constraints and such?
+# What does a schema even do?? It validates??
+# Based on defined rules???
+# Yeah, I guess that's really the whole point?
 
 # Lit that this actually works!
-print(FCValue.of({"a": 1, "b": {"c": 3}}))
+# print(FCValue.of({"a": 1, "b": {"c": 3}}))
 # So this is a fern config object ^ 
 # Which in theory can be parsed directly from JSON?
 # Ehhh, we may want a parse function as is imo...
