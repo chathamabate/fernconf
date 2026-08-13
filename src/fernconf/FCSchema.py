d8b84839b8f4011158e4fe10a375f352b4254c6d
Chatham Abate <chathamabate@gmail.com>
2026-08-12 23:07:02 -0700

    one_of and const (#17)

--- src/fernconf/FCSchema.py
+++ src/fernconf/FCSchema.py
@@ -48,6 +48,67 @@ class FCSchema(ABC):
     def with_extra_checks(self, **checks: Callable[[FCValue], Result[None, str]]) -> FCSchema:
         return FCSchemaWithExtraChecks(self, **checks)
 
+    def const(self, value: FCValue) -> FCSchema:
+        """
+        This Schema will be given `value` as a default value THEN will be given
+        and extra check which refuses all other values!
+        """
+
+        # Frustratingly, this function will actually validate `value` twice.
+        # Once in this function, and a second time when `with_default` executes.
+
+        validated_value_result = self.validate(value)
+        if validated_value_result.is_err():
+            raise Exception(f"Invalid constant value: {validated_value_result.unwrap_err()}")
+        validated_value = validated_value_result.unwrap()
+
+        # Its important to realize the default is changed BEFORE applying the
+        # extra checks. This way `self`'s current default value does not cause
+        # an exception to be thrown if it is not equal to value.
+        #
+        # NOTE: Realize, that the internal default values of `self` still exist!
+        return self.with_default(value).with_extra_checks(
+            check_const=lambda v: Ok(None) if v == validated_value else Err(f"Constant expected: {validated_value}")
+        )
+
+    def const_any(self, value: Any) -> FCSchema:
+        value_result = fcv_of(value)
+        if value_result.is_err():
+            raise Exception(f"Constant value is not FCValue: {value_result.unwrap_err()}")
+
+        return self.const(value_result.unwrap())
+
+    def one_of(self, *values: FCValue) -> FCSchema:
+        """
+        The default overriding behavior is similar to that of `const`.
+
+        NOTE: The FIRST value given will be come the new defualt!
+        For example, self.one_of("a", "b", "c") sets "a" to the new default!
+        """
+        if len(values) == 0:
+            raise Exception("Must be given at least 1 choice for one_of")
+
+        validated_value_results = [self.validate(v) for v in values]
+
+        for index, vvr in enumerate(validated_value_results):
+            if vvr.is_err():
+                raise Exception(f"Invalid choice {index}: {vvr.unwrap_err()}")
+
+        validated_values = [vvr.unwrap() for vvr in validated_value_results]
+        return self.with_default(values[0]).with_extra_checks(
+            check_one_of=lambda v: Ok(None) if v in validated_values else Err(f"Value not one of: {validated_values}")
+        )
+
+    def one_of_any(self, *values: Any):
+        fcv_results = [fcv_of(v) for v in values]
+
+        for index, fcvr in enumerate(fcv_results):
+            if fcvr.is_err():
+                raise Exception(f"Non-FCValue choise {index}: {fcvr.unwrap_err()}")
+
+        fcvs = [fcvr.unwrap() for fcvr in fcv_results]
+        return self.one_of(*fcvs)
+
     @abstractmethod
     def validate(self, value: FCValue) -> Result[FCValue, str]:
         """
