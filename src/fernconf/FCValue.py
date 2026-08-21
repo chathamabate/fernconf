67fa22f7e3b458b8e41e807e38083e048c818b02
Chatham Abate <chathamabate@gmail.com>
2026-08-20 18:52:39 -0700

    Introducing better error messages

--- src/fernconf/FCValue.py
+++ src/fernconf/FCValue.py
@@ -4,6 +4,22 @@ import re
 from typing import Any, override, cast
 from result import Ok, Err, Result
 
+type FCErrorMessage = list[str]
+
+def fcem_line(line: str) -> FCErrorMessage:
+    return [line]
+
+def fcem_prepend_lines(lines: FCErrorMessage, old_lines: FCErrorMessage) -> FCErrorMessage:
+    """
+    Returns a new error message which equals lines + old_lines where old lines are all tabbed
+    over by two spaces.
+    """
+    return lines + ["  " + ol for ol in old_lines]
+
+def fcem_prepend_line(line: str, old_lines: FCErrorMessage) -> FCErrorMessage:
+    return fcem_prepend_lines(fcem_line(line), FCErrorMessage)
+
+
 FC_ID_PATTERN: re.Pattern = re.compile("[A-Za-z_][A-Za-zZ0-9_]*")
 """ 
 A regex pattern which is used often in fernconf to confirm various IDs/Keys follow a 
@@ -21,16 +37,16 @@ Additionally, once you have an FCValue, it should be treated as IMMUTABLE!!!
 Always use `fcv_of`below when creating new FCValues!
 """
 
-def fcv_int_check_result(value: int) -> Result[int, str]:
+def fcv_int_check_result(value: int) -> Result[int, FCErrorMessage]:
     """
     Confirm that an integer can fit into either a 64-bit signed integer, or a 64-bit 
     unsigned integer!
     """
     if value < -0x8000_0000_0000_0000:
-        return Err(f"Given value exceeds 64-bit negative bound {str(value)}")
+        return fcem_line(f"Given value exceeds 64-bit negative bound {str(value)}")
 
     if value >= 0x1_0000_0000_0000_0000:
-        return Err(f"Given value exceends 64-bit unsigned positive bound {str(value)}")
+        return fcem_line(f"Given value exceends 64-bit unsigned positive bound {str(value)}")
 
     return Ok(value)
 
@@ -45,7 +61,7 @@ def fcv_int_check(value: int) -> None:
         raise Exception(res.err())
 
 
-def fcv_of(value: Any) -> Result[FCValue, str]:
+def fcv_of(value: Any) -> Result[FCValue, FCErrorMessage]:
     """
     The purpose of this function is to "construct" a FCValue from an any typed value.
     You may source a value from something which cannot be typechecked before runtime.
@@ -70,26 +86,32 @@ def fcv_of(value: Any) -> Result[FCValue, str]:
                     case Ok(new_val):
                         new_list.append(new_val)
                     case Err(msg):
-                        return Err(f"[{i}] {msg}")
+                        return fcem_prepend_line(
+                            f"Error at list index [{i}]",
+                            msg
+                        )
             return Ok(new_list)
         case dict():
             new_dict: dict[str, FCValue] = {}
             for k, v in value.items():
                 if not isinstance(k, str):
-                    return Err("dict values must only have string keys")
+                    return fcem_line("dict values must only have string keys")
 
                 if not FC_ID_PATTERN.fullmatch(k):
-                    return Err(f"dict key name does not conform to FCValue regex: \"{k}\"")
+                    return fcem_line(f"dict key name does not conform to FCValue regex: \"{k}\"")
 
                 match fcv_of(v):
                     case Ok(new_val):
                         new_dict[k] = new_val
                     case Err(msg):
-                        return Err(f"[{k}] {msg}")
+                        return fcem_prepend_line(
+                            f"Error a dict key [\"{k}\"]",
+                            msg
+                        )
 
             return Ok(new_dict)
         case _:
-            return Err("FCValues must conform to typedef: int | bool | str | list[FCValue] | dict[str, FCValue]")
+            return fcem_line("FCValues must conform to typedef: int | bool | str | list[FCValue] | dict[str, FCValue]")
 
 # NOTE: The below helpers are really just to help with static type checking.
 # If this module didn't use mypy, these functions wouldn't be necessary.
